b652dc90f74b850ed7e9685277ceeefed649d46c
Thái Tấn Lợi <thaitanloi365@gmail.com>
2020-07-08 14:10:39 +0700

    Update latest docker-compose version

--- install_ubuntu_18_04.sh
+++ install_ubuntu_18_04.sh
@@ -27,8 +27,12 @@ install_make() {
 }
 
 install_docker_compose() {
-    echo -e "------> Installing docker compose\n"
-    sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+    # get latest docker compose released tag
+    COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+    
+    echo -e "------> Installing docker compose version ${COMPOSE_VERSION}\n"
+
+    sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 
     sudo chmod +x /usr/local/bin/docker-compose
 
